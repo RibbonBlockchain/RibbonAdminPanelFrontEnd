fc706d0ff99db4c44cdc587751778b6375292584
Kehinde Ayeola (Zoracom - NG) <kehinde@zoracom.com>
2024-10-12 12:18:16 +0100

    fix: mass transfer history

--- src/app/(dashboard)/(routes)/payments/(routes)/history/_components/payment_history_table.tsx
+++ src/app/(dashboard)/(routes)/payments/(routes)/history/_components/payment_history_table.tsx
@@ -3,12 +3,14 @@
 import { useToken } from "@/components/providers/token";
 import { paymentService } from "@/services/payments";
 import { useQuery } from "@tanstack/react-query";
-import { GoArrowDownLeft, GoArrowUpRight } from "react-icons/go";
+import { Spin, Table } from "antd";
+import { format } from "date-fns";
+import { HiOutlineArrowDownLeft, HiOutlineArrowUpRight } from "react-icons/hi2";
 
 const PaymentHistoryPage = () => {
 	const { token } = useToken();
 
-	const { data, isPending, refetch } = useQuery({
+	const { data, isPending } = useQuery({
 		queryKey: ["payment-history"],
 		queryFn: () => paymentService.getPaymentHistory(token || ""),
 		enabled: !!token,
@@ -16,38 +18,107 @@ const PaymentHistoryPage = () => {
 
 	const trx: any = data;
 
+	if (isPending) {
+		return (
+			<div className="flex justify-center">
+				<Spin />
+			</div>
+		);
+	}
+
+	// Merge in and out transfers and label them
+	const mergedData: any[] = [
+		...trx.data.in.transfers.map((item: any) => ({ ...item, type: "in" })),
+		...trx.data.out.transfers.map((item: any) => ({ ...item, type: "out" })),
+	];
+
+	// Sort by blockTimestamp
+	const sortedData = mergedData.sort((a, b) => {
+		return (
+			new Date(a.metadata.blockTimestamp).getTime() -
+			new Date(b.metadata.blockTimestamp).getTime()
+		);
+	});
+
+	// Create the dataSource
+	const dataSource = sortedData.map((item, index) => ({
+		key: (index + 1).toString(),
+		type: item.type,
+		blockNum: item.blockNum,
+		uniqueId: item.uniqueId,
+		hash: item.hash,
+		from: item.from,
+		to: item.to,
+		value: item.value,
+		asset: item.asset,
+		blockTimestamp: item.metadata.blockTimestamp,
+	}));
+
+	const columns = [
+		{
+			title: "Transaction History",
+			dataIndex: "type",
+			key: "type",
+			render: (type: string) => {
+				return (
+					<div>
+						{type === "in" ? (
+							<div className="flex items-center space-x-2">
+								<span className="inline-block rounded-full bg-green-100 p-2">
+									<HiOutlineArrowDownLeft
+										className="text-green-500"
+										size={15}
+									/>
+								</span>
+								<span>
+									<p className="font-semibold">Received</p>
+									<p className="text-xs font-semibold">USDC</p>
+								</span>
+							</div>
+						) : (
+							<div className="flex items-center space-x-2">
+								<span className="inline-block rounded-full bg-red-100 p-2">
+									<HiOutlineArrowUpRight className="text-red-500" size={15} />
+								</span>
+								<span>
+									<p className="font-semibold">Sent</p>
+									<p className="text-xs font-semibold">USDC</p>
+								</span>
+							</div>
+						)}
+					</div>
+				);
+			},
+		},
+		{
+			dataIndex: "address",
+			key: "address",
+			render: (_: any, data: any) => (
+				<p className="font-semibold ">
+					{data.type === "out" ? data.to : data.from}
+				</p>
+			),
+		},
+		{
+			dataIndex: "blockTimestamp",
+			key: "blockTimestamp",
+			render: (_: any, data: any) => (
+				<div>
+					<p className="font-semibold">
+						{data.value} {data.asset}
+					</p>
+					<p className="text-xs">
+						{format(new Date(data.blockTimestamp), "hh:mm a")} |{" "}
+						{format(new Date(data.blockTimestamp), "do MMM, yyyy")}{" "}
+					</p>
+				</div>
+			),
+		},
+	];
+
 	return (
 		<div className="p-4">
-			{trx &&
-				trx.data.in.transfers.map((tx: any, index: number) => (
-					<div
-						key={index}
-						className="flex items-center justify-between border-b border-gray-200 p-4"
-					>
-						{/* Left Section */}
-						<div className="flex items-center space-x-2">
-							<div
-								// className={`text-xl ${tx.sent ? "text-red-500" : "text-green-500"}`}
-								className={`text-xl text-red-500`}
-							>
-								{/* {tx.sent ? <GoArrowUpRight /> : <GoArrowDownLeft />} */}
-								<GoArrowUpRight />
-							</div>
-							<div>
-								<div className="text-sm font-semibold">Sent</div>
-								<div className="text-xs text-gray-500">{tx.to}</div>
-							</div>
-						</div>
-
-						{/* Right Section */}
-						<div className="text-right">
-							<div className="text-sm font-semibold">
-								{tx.value} {tx.asset}
-							</div>
-							<div className="text-xs text-gray-500">{tx.asset}</div>
-						</div>
-					</div>
-				))}
+			<Table dataSource={dataSource} columns={columns} loading={isPending} />
 		</div>
 	);
 };
